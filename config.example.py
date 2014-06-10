7416b339e0e8f805deda308e15703c15fd0e6842
alix <fullertonalix0@gmail.com>
2014-06-10 11:47:58 -0600

    Updating configuration file

--- config.example.py
+++ config.example.py
@@ -1,34 +1,35 @@
+#!/usr/bin/python
 """
-  PyPolix config
+  PyPollex Config File
   rename file to config.py before use
 """
 config = {
   'verbosity'     : True,
   'use_cache'     : True,
-  'upload_dir'    : '/home/user/PyPollex/images/uploads/',
-  'cache_dir'     : '/home/user/PyPollex/images/cache/',
-  'log_dir'       : '/home/user/PyPollex/PyThumbs/logs/',
-  'use_whitelist' : False,
-  'whitelist'     : [],
-  'use_blacklist' : False,
-  'blacklist'  : [
-    'imgur.com',
-    'flickr.com'
-  ],
+  'img_dir'       : '/home/alix/Dropbox/Python-Fun/PyPolex/images/',
+  'log_dir'       : '/home/alix/Dropbox/Python-Fun/PyPolex/logs/',
+  'whitelist'     : [ 'vailrealestate.com' ],
+  'blacklist'     : [ 'imgur.com', 'flickr.com' ],
+  'database'      : {
+    'host'   : 'localhost',
+    'user'   : 'user',
+    'pass'   : 'password',
+    'dbname' : 'PyPolex'
+  },
   'webserver'     : {
-      'global': {
-        'server.socket_port'          : 8099,
-        'server.socket_host'          : "10.1.10.55",
-        'server.socket_file'          : "",
-        'server.socket_queue_size'    : 5,
-        'server.protocol_version'     : "HTTP/1.0",
-        'server.log_to_screen'        : True,
-        'server.log_file'             : "logs/server.log",
-        'server.reverse_dns'          : False,
-        'server.thread_pool'          : 40,
-        'server.environment'          : "development",
+    'global': {
+      'server.socket_port'          : 9000,
+      'server.socket_host'          : "192.168.1.x",
+      'server.socket_file'          : "",
+      'server.socket_queue_size'    : 5,
+      'server.protocol_version'     : "HTTP/1.0",
+      'server.log_to_screen'        : True,
+      'server.log_file'             : "logs/server.log",
+      'server.reverse_dns'          : False,
+      'server.thread_pool'          : 40,
+      'server.environment'          : "development",
     },
-  }  
+  }
 }
 
-# End File: config.example.py
+# End File: config.py
